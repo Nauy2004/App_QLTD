15dab6371dec5f6650785687327c100c9aa8e759
Nauy2004 <tuanlmht77@gmail.com>
2024-06-28 02:26:09 +0700

    Add project files.

---
+++ GUI/loging.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class loging : Form
+    {
+        public loging()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2HtmlLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBoxShowPass_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void elementHost1_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+        {
+
+        }
+
+        private void guna2TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void enter_input(object sender, EventArgs e)
+        {
+            textBox_tk.Text = "";
+            textBoxMk.Text = "";
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (textBox_tk.Text == "ADMIN" && textBoxMk.Text == "123")
+            {
+                Home home = new Home();
+                home.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("?");
+            }
+        }
+    }
+}
